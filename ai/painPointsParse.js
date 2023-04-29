93983684bff2c2b219ae486ce67fa94b13b6d0ed
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2023-04-29 16:56:03 -0400

    parse pain points

--- ai/painPointsParse.js
+++ ai/painPointsParse.js
@@ -1,6 +1,7 @@
 import { log, bold, blue } from "./utils/log.js";
 
 import fs from "fs";
+import slugify from "slugify";
 
 import * as sqlite from "./db/index.js";
 const DB_FILE = "./db/DB.db";
@@ -16,26 +17,31 @@ try {
 	const painPointsFiles = await fs.promises.readdir("./pain_points");
 
 	for (const file of painPointsFiles) {
-		const painPoint = JSON.parse(await fs.promises.readFile(`./pain_points/${file}`, "utf8"));
+		const data = JSON.parse(await fs.promises.readFile(`./pain_points/${file}`, "utf8"));
 
 		// Skip if not new
-		if (painPoint.status !== PAIN_POIN_STATUS.NEW) continue;
+		if (data.status !== PAIN_POIN_STATUS.NEW) continue;
 
-		const dataTable = parseTable(painPoint.content);
+		const dataTable = parseTable(data.content);
 
 		for (const results of dataTable) {
+			let description = results[1];
+			while (description[description.length - 1] === ".") description = description.slice(0, -1);
+			
 			const query = {
-				audience_id: painPoint.audience_id,
+				audience_id: data.audience_id,
 				title: results[0].toLowerCase(),
 				slug: slugify(results[0], { remove: ":", lower: true, trim: true }),
-				description: results[1].toLowerCase(),
+				description: description.toLowerCase(),
 			};
-			console.log(`${blue(query.audience)}\n${query.title}\n${bold(query.description)}`);
+			console.log(
+				`${blue(`${data.audience_id} - ${data.audience_title}`)}\n${query.title}\n${bold(query.description)}`,
+			);
 			await sqlite.insertRow("pain_points", query);
 		}
 
-		painPoint.status = PAIN_POIN_STATUS.PARSED;
-		await fs.promises.writeFile(`./pain_points/${painPoint.audience_slug}.json`, JSON.stringify(data, null, 2));
+		data.status = PAIN_POIN_STATUS.PARSED;
+		await fs.promises.writeFile(`./pain_points/${data.audience_slug}.json`, JSON.stringify(data, null, 2));
 	}
 
 	await sqlite.close();
