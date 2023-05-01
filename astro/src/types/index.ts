fd30f0c77cfd0667211be8e7c60ba0c2fcdf0449
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2023-05-01 02:56:09 -0400

    add a tons of changes

--- astro/src/types/index.ts
+++ astro/src/types/index.ts
@@ -1,29 +1,67 @@
+export interface DataItem {
+	id: number;
+	title: string;
+	slug: string;
+	pain_points: PainPoint[];
+}
+
 export interface Audience {
 	id: number;
 	title: string;
 	slug: string;
 }
-
 export interface PainPoint {
 	id: number;
 	title: string;
-    slug: string;
-    description: string;
-    status: string;
-    solutions: Solution[];
+	slug: string;
+	description: string;
+	status: string;
+	solutions: Solution[];
 }
 
 export interface Solution {
 	id: number;
-    title: string;
-    description: string;
-    slug: string;
-    features: string;
-    competitors: string;
-    differentiator: string;
+	title: string;
+	description: string;
+	slug: string;
+	features: string;
+	competitors: string;
+	differentiator: string;
+}
+export interface SlugAudiences {
+	params: {
+		audiences: string;
+	};
+	props: {
+		id: number;
+		title: string;
+		slug: string;
+	};
 }
 
-export interface DataItem {
-	audience: Audience;
-	pain_points: PainPoint[];
+export interface SlugAudiencesPainPoints {
+	params: {
+		audience: string;
+		pain_point: string;
+	};
+	props: {
+		audience: Audience;
+		pain_point: PainPoint;
+		solutions: Solution[];
+	};
 }
+
+export interface SlugAudiencesPainPointsSolutions {
+	params: {
+		audience: string;
+		pain_point: string;
+		solution: string;
+	};
+	props: {
+		audience: Audience;
+		pain_point: PainPoint;
+		solution: Solution;
+	};
+}
+
+
